ccafd00ebe9b30cb5a69fe0cb87a62c20d018873
Sihan Ma <masihan0303@gmail.com>
2025-04-12 05:29:49 +0100

    Setup S3 Bucket, SNS Topic, SQS Queue, and Dead Letter Queue (DLQ) for image upload flow

--- lib/photo-gallery-app-stack.ts
+++ lib/photo-gallery-app-stack.ts
@@ -1,16 +1,46 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as sns from 'aws-cdk-lib/aws-sns';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as sns_subscriptions from 'aws-cdk-lib/aws-sns-subscriptions';
+import * as s3_notifications from 'aws-cdk-lib/aws-s3-notifications';
 
 export class PhotoGalleryAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const photoBucket = new s3.Bucket(this, 'PhotoBucket', {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'PhotoGalleryAppQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const photoTopic = new sns.Topic(this, 'PhotoTopic');
+
+    const deadLetterQueue = new sqs.Queue(this, 'DeadLetterQueue');
+
+    const photoQueue = new sqs.Queue(this, 'PhotoQueue', {
+      deadLetterQueue: {
+        queue: deadLetterQueue,
+        maxReceiveCount: 3,
+      }
+    });
+
+    photoTopic.addSubscription(new sns_subscriptions.SqsSubscription(photoQueue));
+
+    photoBucket.addEventNotification(
+      s3.EventType.OBJECT_CREATED,
+      new s3_notifications.SnsDestination(photoTopic)
+    );
+
+    new cdk.CfnOutput(this, 'PhotoBucketName', {
+      value: photoBucket.bucketName,
+    });
+    new cdk.CfnOutput(this, 'PhotoTopicArn', {
+      value: photoTopic.topicArn,
+    });
+    new cdk.CfnOutput(this, 'PhotoQueueUrl', {
+      value: photoQueue.queueUrl,
+    });
   }
 }
